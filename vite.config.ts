e7e66e1057e04d4e519772b9f3c8517ef303cb95
honlnk <jihongshen1@outlook.com>
2026-05-01 22:39:40 +0800

    chore: add Vue DevTools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
 })
