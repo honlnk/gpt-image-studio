04a9b8b9e7b99a8caca70e0ddcde79a2705e91db
honlnk <jihongshen1@outlook.com>
2026-05-09 23:42:35 +0800

    ci: add quality checks to workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: './',
   server: {
     host: '127.0.0.1',
     port: 8888,
   },
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-})
+  plugins: [
+    vue(),
+    mode === 'development' && vueDevTools(),
+    tailwindcss(),
+  ].filter(Boolean),
+}))
